2e324f0238b8acf76755cdb99091f03ee7952a01
NicolasGraph <contact@nicolasmorand.com>
2016-10-28 14:47:15 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,38 @@
+# oui_page
+
+Easily manipulate the Textpattern pages name.
+
+## Plugin requirements
+
+oui_instagram’s minimum requirements:
+
+* Textpattern 4.6+
+
+## Installation
+
+1. [Download](https://github.com/NicolasGraph/oui_page/releases) the plugin;
+1. Paste the content of the plugin file under the *Admin > Plugins*, upload it and install;
+
+## Tag
+
+### oui_page
+
+Displays the page name in use for the current section or a defined section.
+
+bc. <txp:oui_page />
+
+#### Attribute
+
+*section*
+: The section name for which you want to display the page name.
+: *Default:* the current section
+: *Valid:* a section name
+
+## Author
+
+[Nicolas Morand](https://twitter.com/NicolasGraph)
+*Thank you to the Textpattern community and the core team.*
+
+## Licence
+
+This plugin is distributed under [GPLv2](http://www.gnu.org/licenses/gpl-2.0.fr.html).
---
+++ oui_page.php
@@ -0,0 +1,85 @@
+<?php
+
+Txp::get('\Textpattern\Tag\Registry')->register('oui_page');
+
+/**
+ * Gets a section's page.
+ *
+ * @param  string $section The section
+ * @return string|bool The page or FALSE on error
+ */
+
+function oui_fetch_section_page($section)
+{
+    static $secpages = array();
+    global $thissection;
+
+    // Try cache.
+    if (isset($secpages[$section])) {
+        return $secpages[$section];
+    }
+
+    // Try global set by section_list().
+    if (!empty($thissection['page']) && $thissection['name'] == $section) {
+        $secpages[$section] = $thissection['page'];
+
+        return $thissection['page'];
+    }
+
+    if (empty($section)) {
+        return '';
+    }
+
+    $f = safe_field("page", 'txp_section', "name = '".doSlash($section)."'");
+    $secpages[$section] = $f;
+
+    return $f;
+}
+
+// -------------------------------------------------------------
+
+function oui_page($atts, $thing = null)
+{
+    global $thisarticle, $s, $thissection;
+
+    extract(lAtts(array(
+        'class'   => '',
+        'section' => '',
+        'wraptag' => '',
+    ), $atts));
+
+    if ($section) {
+        $sec = $section;
+    } elseif (!empty($thissection['name'])) {
+        $sec = $thissection['name'];
+    } elseif (!empty($thisarticle['section'])) {
+        $sec = $thisarticle['section'];
+    } else {
+        $sec = $s;
+    }
+
+    if ($sec) {
+        $out = oui_fetch_section_page($sec);
+
+        return doTag($out, $wraptag, $class);
+    }
+}
+
+// -------------------------------------------------------------
+
+function oui_if_page($atts, $thing)
+{
+    global $pretext;
+    extract($pretext);
+
+    extract(lAtts(array(
+        'name' => false,
+        'section' => '',
+    ), $atts));
+
+    if ($section) {
+        $page = oui_page(array('section' => $section));
+    }
+
+    return parse($thing, $name === false or in_list($page, $name));
+}
